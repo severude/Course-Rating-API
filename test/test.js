20f4cfd0ab2dc103f685e6dcb78267e54b9c82e5
Dale Severude <dseverude@msn.com>
2018-11-15 22:41:17 -0800

    Adding unit tests

--- test/test.js
+++ test/test.js
@@ -1,13 +1,24 @@
+const express = require('express');
 const mongoose = require("mongoose");
+const mid = require('../src/middleware');
 mongoose.Promise = global.Promise;
 const expect = require('chai').expect;
-const sinon = require('sinon');
 const User = require('../src/models').User;
+const request = require('superagent');
+const app = express();
 
 describe('Verify user credentials', () => {
 
+    const userCredentials = {
+        emailAddress: "john@adams.com", 
+        password: "password"
+    }
+    const badCredentials = {
+        emailAddress: "jon@adams.com", 
+        password: "password"
+    }
+
     before( (done) => {
-        // Note: Mongod must be running for this to work!
         mongoose.connect("mongodb://localhost:27017/course-api", { useNewUrlParser: true } );
 
         // Verify mongo database connection
@@ -37,9 +48,25 @@ describe('Verify user credentials', () => {
     });
 
     it('should return corresponding user document with correct user credentials', () => {
+        request.agent(app)
+            .get('/api/users')
+            .send(userCredentials)
+            .end((err, res) => {
+                if(err) throw err;
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
     });
 
     it('should return 401 error with incorrect user credentials', () => {
+        request.agent(app)
+            .get('/api/users')
+            .send(badCredentials)
+            .end((err, res) => {
+                if(err) throw err;
+                expect(res.statusCode).to.equal(401);
+                done();
+            });
     });
 
     after( (done) => {
